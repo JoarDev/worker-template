acf9668164005e715fff8667969456298b2aba1f
Joaquin Ignacio Ramos <j.ramos5452@gmail.com>
2022-11-02 03:22:59 +0000

    basic toggl functionality

--- index.js
+++ index.js
@@ -6,7 +6,34 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+
+  const workspaceId = 4516727
+  const toggl_url = `https://api.track.toggl.com/api/v9/workspaces/${workspaceId}/time_entries`
+  
+  const nowDate = new Date()
+  let body = JSON.stringify({
+    "start": nowDate.toISOString(),
+    "duration": (-1) * Math.floor(nowDate.getTime() / 1000),
+    "created_with": "cloudflare worker",
+    "pid": 186782977,
+    "wid": workspaceId
+  })
+
+  let token = btoa(TOGGL_API_KEY + ':api_token');
+
+  const requestInit = {
+    body: body,
+    method: 'POST',
+    headers: {
+      'Authorization': `Basic ${token}`,
+      'Content-Type': 'application/json',
+    },
+  };
+
+  return fetch(toggl_url, requestInit)
+  .then((response) => {
+    return new Response(JSON.stringify(response.status), {
+      headers: { 'content-type': 'text/plain' },
+    })
   })
 }
